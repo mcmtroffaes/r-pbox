0f009ff2193cef6fcf77530f29a4583e03277ce2
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2018-10-02 15:10:10 +0100

    Started documenting things.

--- R/pbox.R
+++ R/pbox.R
@@ -1,75 +1,109 @@
-# a cdf sequence is a sequence of numbers x1 ... xn so that
-# cdf(x)=0   for x<x1
-# cdf(x)=1/n for x1<=x<x2
-# cdf(x)=2/n for x2<=x<x3
-# ...
-# cdf(x)=1   for xn<=x
-
-check_discrete_cdf <- function(object) {
-  if (length(object@points) < 1L) {
-    "must contain at least one point"
-  } else if (is.unsorted(object@points)) {
-    "points must be sorted"
-  } else {
-    TRUE
+#' An S4 class to represent a discrete cumulative distribution function.
+#'
+#' This class represents a probability mass function over n points on
+#' the real line, where the probability masses are uniformly
+#' distributed.
+#'
+#' @slot points A sorted numeric vector of length one or more
+#' @export
+setClass(
+  "DiscreteCdf",
+  representation(points = "numeric"),
+  prototype(points = 0),
+  validity = function(object) {
+    if (length(object@points) < 1L) {
+      "must contain at least one point"
+    } else if (is.unsorted(object@points)) {
+      "points must be sorted"
+    } else {
+      TRUE
+    }
   }
-}
-
-setClass("DiscreteCdf", representation(points = "numeric"),
-  list(points = 0),
-  validity = check_discrete_cdf
 )
 
-setMethod("show", "DiscreteCdf", function(object) cat("points for cdf =", object@points, "\n"))
-
+#' Create a new DiscreteCdf from a given set of sorted points.
+#'
+#' @param points A sorted numeric vector of length one or more
+#' @export
 discrete_cdf <- function(points) new("DiscreteCdf", points = points)
 
+#' Number of points.
+#' @export
 setMethod("length", "DiscreteCdf", function(x) length(x@points))
 
+#' Distribution mean.
+#' @export
 setMethod("mean", "DiscreteCdf", function(x) mean(x@points))
 
+#' Distribution median.
+#' @export
 setMethod("median", "DiscreteCdf", function(x) median(x@points))
 
-# calculate the cdf as a function of x according the above
-# discrete representation (note: implementation is simple but slow)
-eval_discrete_cdf <- function(cdf) function(x) {
+#' Create cumulative distribution function.
+eval_discrete_cdf <- function(cdf) {
+  function(x) {
+    # (note: implementation is simple but slow)
     n <- length(cdf)
     for (i in 1:n) {
       if (x < cdf@points[i]) return((i - 1) / n)
     }
     return(1)
   }
+}
 
 # a p-box is represented by two cdf sequences
 # u@points[1] ... u@points[n]
 # l@points[1] ... l@points[n]
 
-check_discrete_pbox <- function(object) {
-  if (length(object@u) != length(object@l)) {
-    "ucdf and lcdf must have same length"
-  } else if (any(object@u@points > object@l@points)) {
-    "ucdf must be left of lcdf"
-  } else {
-    TRUE
+#' An S4 class to represent a discrete p-box.
+#'
+#' This class represents the set of all cumulative distribution
+#' functions contained between two discrete cumulative distribution
+#' functions.
+#'
+#' @slot u Upper discrete cumulative distribution function
+#' @slot l Lower discrete cumulative distribution function
+#'
+#' @export
+setClass(
+  "DiscretePBox",
+  representation(u = "DiscreteCdf", l = "DiscreteCdf"),
+  validity = function(object) {
+    if (length(object@u) != length(object@l)) {
+      "ucdf and lcdf must have same length"
+    } else if (any(object@u@points > object@l@points)) {
+      "ucdf must be left of lcdf"
+    } else {
+      TRUE
+    }
   }
-}
-
-setClass("DiscretePBox", representation(u = "DiscreteCdf", l = "DiscreteCdf"),
-  validity = check_discrete_pbox
 )
 
+#' Create a new DiscretePBox from two sets of sorted points.
+#'
+#' @param u A sorted numeric vector of length one or more,
+#'     representing the upper cdf
+#' @param l A sorted numeric vector of length one or more,
+#'     representing the lower cdf
+#'
+#' @export
 discrete_pbox <- function(upoints, lpoints)
   new("DiscretePBox", u = discrete_cdf(upoints), l = discrete_cdf(lpoints))
 
+#' Number of points.
+#'
+#' @export
 setMethod("length", "DiscretePBox", function(x) length(x@u@points))
 
-setMethod("show", "DiscretePBox", function(object) {
-  cat("points for upper cdf =", object@u@points, "\n")
-  cat("points for lower cdf =", object@l@points, "\n")
-})
-
-# bot = bottom quantile (cut off at 1/steps)
-# top = top quantile (cut off at 1-1/steps)
+#' Create a discrete pbox bounding a given normal distribution.
+#'
+#' @param mean Mean
+#' @param sd Standard deviation
+#' @param bot Quantile for left cut-off point (should be less than 1/steps)
+#' @param top Quantile for right cut-off point (should be more than 1-1/steps)
+#' @param steps Number of discretization steps
+#'
+#' @export
 discrete_pbox_norm <- function(mean = 0, sd = 1, bot = 0.001, top = 0.999, steps = 200) {
   stopifnot(bot >= 0, top <= 1)
   ps <- c(
@@ -81,6 +115,13 @@ discrete_pbox_norm <- function(mean = 0, sd = 1, bot = 0.001, top = 0.999, steps
   discrete_pbox(upoints = points[-(steps + 1)], lpoints = points[-1])
 }
 
+#' Create a discrete pbox bounding a given uniform distribution.
+#'
+#' @param min Left side of the range
+#' @param max Right side of the range
+#' @param steps Number of discretization steps
+#'
+#' @export
 discrete_pbox_unif <- function(min = 0, max = 1, steps = 200) {
   ps <- seq(0, 1, length.out = steps + 1)
   points <- sapply(ps, function(x) qunif(x, min = min, max = max))
@@ -193,16 +234,22 @@ discrete_pbox_convolution <- function(func) function(pbox1, pbox2) {
     )
   }
 
-setMethod("+", "DiscretePBox", function(e1, e2) discrete_pbox_convolution(`+`)(e1, e2))
+setGeneric("%iadd%", function(e1, e2) standardGeneric("%iadd%"))
+setGeneric("%imul%", function(e1, e2) standardGeneric("%imul%"))
+setGeneric("%isub%", function(e1, e2) standardGeneric("%isub%"))
+setGeneric("%idiv%", function(e1, e2) standardGeneric("%idiv%"))
 
-setMethod("*", "DiscretePBox", function(e1, e2) {
+setMethod("%iadd%", "DiscretePBox",
+          function(e1, e2) discrete_pbox_convolution(`+`)(e1, e2))
+
+setMethod("%imul%", "DiscretePBox", function(e1, e2) {
   stopifnot(e1 >= 0, e2 >= 0)
   discrete_pbox_convolution(`*`)(e1, e2)
 })
 
-setMethod("-", "DiscretePBox", function(e1, e2) e1 + (-e2))
+setMethod("%isub%", "DiscretePBox", function(e1, e2) e1 + (-e2))
 
-setMethod("/", "DiscretePBox", function(e1, e2) e1 * (1 / e2))
+setMethod("%idiv%", "DiscretePBox", function(e1, e2) e1 * (1 / e2))
 
 # implementation of Williamson & Downs, page 120-121 (cases i and ii)
 discrete_pbox_frechet <- function(func) function(pbox1, pbox2) {
@@ -249,14 +296,14 @@ setMethod("plot", "DiscretePBox", function(x, xlab = "x", ylab = "cumulative pro
 })
 
 test1 <- function() {
-  pb <- discrete_pbox_norm(n = 10)
+  pb <- discrete_pbox_norm(steps = 10)
   plot(pb)
   xs <- seq(-5, 5, 0.01)
   lines(xs, sapply(xs, pnorm), type = "l", col = 3)
 }
 
 test2 <- function() {
-  pb <- discrete_pbox_unif(min = -2, max = 3, n = 10)
+  pb <- discrete_pbox_unif(min = -2, max = 3, steps = 10)
   plot(pb)
   xs <- seq(-4, 4, 0.01)
   lines(xs, sapply(xs, function(x) punif(x, min = -2, max = 3)), type = "l", col = 3)
@@ -264,16 +311,16 @@ test2 <- function() {
 
 # W&D figure 19
 test3 <- function() {
-  pb1 <- discrete_pbox_unif(min = 1, max = 2, n = 40)
-  pb2 <- discrete_pbox_unif(min = 1, max = 2, n = 40)
-  pb3 <- pb1 + pb2
+  pb1 <- discrete_pbox_unif(min = 1, max = 2, steps = 40)
+  pb2 <- discrete_pbox_unif(min = 1, max = 2, steps = 40)
+  pb3 <- pb1 %iadd% pb2
   plot(pb3)
 }
 
 # W&D figure 21
 test4 <- function() {
-  pb1 <- discrete_pbox_unif(min = 0, max = 1, n = 40)
-  pb2 <- discrete_pbox_unif(min = 0, max = 1, n = 40)
+  pb1 <- discrete_pbox_unif(min = 0, max = 1, steps = 40)
+  pb2 <- discrete_pbox_unif(min = 0, max = 1, steps = 40)
   pb3 <- pb1 %fadd% pb2
   plot(pb3)
 }
--- prep.R
+++ prep.R
@@ -1,2 +1,5 @@
+library("roxygen2")
 library("styler")
+
+roxygenize()
 style_file("R/pbox.R")
